46483d7829a929f66faa94406f6cc46ad4290759
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2024-09-20 12:59:09 +0200

    a

--- wolf_comm/wolf_client.py
+++ wolf_comm/wolf_client.py
@@ -128,7 +128,7 @@ class WolfClient:
                 #     val.name = name
                 spaceSplit = val.name.split(' ', 2)
                 if len(spaceSplit) == 2:
-                    key = spaceSplit[0].split('_')[1]
+                    key = spaceSplit[0].split('_')[1] if spaceSplit[0].count('_') > 0 else spaceSplit[0]
                     name = self.replace_with_localized_text(key) + ' ' + spaceSplit[1]
                     val.name = name
 
